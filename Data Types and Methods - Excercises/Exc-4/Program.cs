f468b25ff05b2efcf04b31cfbfa9e7d335c54a85
Yordan Dimitrov <dimitrov.yordan91@gmail.com>
2016-09-24 19:10:43 +0300

    Up to Exc-4.

---
+++ Data Types and Methods - Excercises/Exc-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+//Variables in Hexadecimal Format
+//
+namespace Exc_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Convert.ToInt32(Console.ReadLine(),16);
+            Console.WriteLine(num);
+        }
+    }
+}
+//Ragequit
